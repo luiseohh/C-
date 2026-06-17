699931adaf9222aa06974a283d22b20438cc6524
luiseohh <luise040420@gmail.com>
2026-06-17 17:49:24 +0900

    if_vs_ternary_ex

---
+++ TextBook/Chapter_02/90p_if_vs_ternary_2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int a = 7;
+    int b = 5;
+    int result = (a > b) ? a : b;
+
+    cout << "result = " << result << endl;
+
+    return 0;
+}
