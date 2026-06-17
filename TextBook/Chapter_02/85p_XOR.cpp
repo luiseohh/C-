e5fb1964a43133efc8a551716f64f3cfc88e91ce
luiseohh <luise040420@gmail.com>
2026-06-17 17:29:50 +0900

    XOR_ex

---
+++ TextBook/Chapter_02/85p_XOR.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<bitset> // bitset<>() 때문에
+using namespace std;
+
+int main(){
+    int a = 13;
+    int b = 27;
+    int c = a ^ b; // 비트 XOR 연산
+    
+    cout << "a = " << bitset<8>(a) << " : " << a << endl;
+    cout << "b = " << bitset<8>(b) << " : " << b << endl;
+    cout << "c = " << bitset<8>(c) << " : " << c << endl;
+
+    // bitset<8>(a) -> 내가 가진 데이터 A를 0과 1로 이루어진 '8자리 2진수(비트)' 모양으로 변신시켜서 보여줘
+    return 0;   
+}
