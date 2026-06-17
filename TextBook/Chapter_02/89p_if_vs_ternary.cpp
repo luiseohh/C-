ab3d1fec6908ffcea67d05fc16411469f28c3513
luiseohh <luise040420@gmail.com>
2026-06-17 17:46:39 +0900

    if_vs_ternary_ex

---
+++ TextBook/Chapter_02/89p_if_vs_ternary.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int a = 7;
+    int b = 5;
+    int result;
+
+    if (a > b){
+        result = a;
+    } else{
+        result = b;
+    }
+
+    cout << "result = " << result << endl;
+
+    return 0;
+}
