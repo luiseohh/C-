1ea7aacf17b6efbd2546eba2de16e68d65d120de
luiseohh <luise040420@gmail.com>
2026-06-18 15:43:20 +0900

    addresss_fixed_array_ex

---
+++ TextBook/Chapter_03/107p_address_fixed_array.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int lotto[45];
+
+    for(int i = 0; i < 45; i++){
+        lotto[i] = i + 1;
+    }
+
+    cout << "lotto[0] Address : " << &lotto[0] << endl;
+    cout << "lotto[1] Address : " << &lotto[1] << endl;
+    cout << "lotto[2] Address : " << &lotto[2] << endl;
+    cout << "lotto[3] Address : " << &lotto[3] << endl;
+    cout << "lotto[4] Address : " << &lotto[4] << endl;
+    cout << "lotto[5] Address : " << &lotto[5] << endl;
+
+    return 0;
+}
