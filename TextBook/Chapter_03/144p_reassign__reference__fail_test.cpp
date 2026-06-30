176b14da74053ca767877e0d8e757a2b6af445ad
luiseohh <luise040420@gmail.com>
2026-06-30 15:03:46 +0900

    reassing_reference_fail_test_ex

---
+++ TextBook/Chapter_03/144p_reassign__reference__fail_test.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int value = 10;
+    int &ref_value = value;
+
+    int value2 = 20;
+    ref_value = value2;
+
+    cout << "value : " << value << endl;
+    cout << "ref_value : " << ref_value << endl;
+
+    return 0;
+}
