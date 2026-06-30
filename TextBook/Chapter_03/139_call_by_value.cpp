5cab9c08c3ca281d17ecd44ec1151a90aa4c6f8a
luiseohh <luise040420@gmail.com>
2026-06-30 14:42:00 +0900

    call_by_value_ex

---
+++ TextBook/Chapter_03/139_call_by_value.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+using namespace std;
+
+void swap(int a, int b){
+    cout << "[swap func] before swap, a : " << a << " b : " << b << endl;
+
+    int temp = a;
+    a = b;
+    b = temp;
+
+    cout << "[swap func] after swap, a : " << a << " b : " << b << endl;
+}
+
+int main(){
+    int a = 5;
+    int b = 10;
+
+    cout << "[main] before swap, a : " << a << " b : " << b << endl;
+
+    swap(a, b);
+
+    cout << "[main] after swap, a : " << a << " b : " << b << endl;
+
+    return 0;
+}
