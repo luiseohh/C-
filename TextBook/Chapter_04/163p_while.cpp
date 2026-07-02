4b0ecb24bb01aa1c412926d282fe05a18476119e
luiseohh <luise040420@gmail.com>
2026-07-02 14:43:39 +0900

    while_ex

---
+++ TextBook/Chapter_04/163p_while.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int count = 0;
+    
+    while(count < 5){
+        cout << "cout : " << count << endl;
+        count++;
+    }
+
+    return 0;
+}
