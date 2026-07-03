3854344a8abea453375be83dc8f9dd8c957e66f0
luiseohh <luise040420@gmail.com>
2026-07-03 13:47:34 +0900

    try_throw_catch_2_ex

---
+++ TextBook/Chapter_05/180p_try_throw_catch_2.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    try{
+        int input;
+        cout << "enter : ";
+        cin >> input;
+
+        if(input > 0){
+            cout << "throw 1" << endl;
+            throw 1;
+            cout << "after throw 1" << endl;
+        } else if(input < 0){
+            cout << "throw -0.1f" << endl;
+            throw -0.1f;
+            cout << "after throw 1" << endl;
+        } else if(input == 0){
+            cout << "throw Z" << endl;
+            throw 'Z';
+            cout << "after throw Z" << endl;
+        }
+    } catch(...){
+        cout << "catch all" << endl;
+    }
+
+    return 0;
+}
