3d21f4a091bf0a2b31dd7eaf47d074eda32885da
luiseohh <luise040420@gmail.com>
2026-07-03 14:49:15 +0900

    noexcept_ex

---
+++ TextBook/Chapter_05/191p_noexcept.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+void real_noexcept() noexcept{
+    cout << "real_noexcept\n";
+}
+
+void fake_noexcept() noexcept{
+    cout << "fake_noexcept\n";
+    throw 1; // 정수 형식 예외 발생
+}
+
+int main(){
+    real_noexcept();
+
+    try{
+        fake_noexcept();
+    } catch(int exec){
+        cout << "catch " << exec << endl;
+    }
+
+    return 0;
+}
