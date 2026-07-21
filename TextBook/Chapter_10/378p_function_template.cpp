b5138696e30ca3942414f4d18edd02d54571c85a
Blurraey <ohluise0420@gmail.com>
2026-07-21 12:50:26 +0900

    function_template_ex

---
+++ TextBook/Chapter_10/378p_function_template.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+template<typename T>
+T data_sum(T operand1, T operand2){
+    return operand1 + operand2;
+}
+
+int main(){
+    int data1 = 3, data2 = 5;
+    double data3 = 4.5, data4 = 8.9;
+    string data5 = "Hello", data6 = "World";
+
+    cout << "int data's sum : " << data_sum(data1, data2) << endl;
+    cout << "double data's sum : " << data_sum(data3, data4) << endl;
+    cout << "string data's sum : " << data_sum(data5, data6) <<endl;
+
+    return 0;
+}
