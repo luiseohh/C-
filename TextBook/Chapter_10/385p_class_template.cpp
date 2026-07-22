79729d9339f5aa535d15b01c750c83f53e5036d4
Blurraey <ohluise0420@gmail.com>
2026-07-22 11:25:02 +0900

    class_template_ex

---
+++ TextBook/Chapter_10/385p_class_template.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+using namespace std;
+
+template<typename Type1, typename Type2>
+class data_package{
+public:
+    data_package(Type1 fir, Type2 sec) : fir(fir), sec(sec){}
+    void print_out_element(){
+        cout << "first elelment : " << fir << ", second element : " << sec << endl;
+    }
+private:
+    Type1 fir;
+    Type2 sec;
+};
+
+int main(){
+    data_package<int, double> template_inst1(5, 10.5);
+    data_package<string, int> template_inst2("string", 10);
+
+    template_inst1.print_out_element();
+    template_inst2.print_out_element();
+
+    return 0;
+}
