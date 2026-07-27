4373324cf2a156e3fdac508dd7ec78cc74c7de06
Blurraey <ohluise0420@gmail.com>
2026-07-27 11:18:13 +0900

    random_device_ex

---
+++ TextBook/Chapter_11/432_random_device.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<random>
+
+using namespace std;
+
+int main(){
+    random_device rng;
+
+    for(int i = 0; i < 10; i++){
+        auto result = rng();
+        cout << result << endl;
+    }
+
+    return 0;
+}
