c468773edbba9c012d8b0163b3bb8069a87f3dde
Blurraey <ohluise0420@gmail.com>
2026-08-07 14:28:10 +0900

    excution_policy2_ex

---
+++ TextBook/Chapter_14/541p_excution_policy2.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<memory>
+
+using namespace std;
+
+class class_obj{
+public:
+    class_obj(){
+        cout << "Memory is allocated\n";
+    };
+    ~class_obj(){
+        cout << "Memory is deleted\n";
+    };
+
+    void print_out(){
+        cout << "Call an object function\n";
+    };
+};
+
+int main(){
+    unique_ptr<class_obj> unique_pointer(new class_obj);
+    unique_pointer->print_out();
+
+    return 0;
+}
