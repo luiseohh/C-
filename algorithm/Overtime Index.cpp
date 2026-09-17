3dc959da19e4aa86162c248485c891f0e030bae3
Blurraey <ohluise0420@gmail.com>
2026-09-17 17:13:54 +0900

    Overtime_index_ex

---
+++ algorithm/Overtime Index.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<queue>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+
+long long solution(int n, vector<int> works){
+    priority_queue<int> pq;
+
+    // filling pq
+    for(auto factor : works){
+        pq.push(factor);
+    }
+
+    while(n > 0 && !pq.empty()){
+        int max_work = pq.top();
+        pq.pop();
+
+        // if pq index is <= 0 => break;
+        if(max_work <= 0) break;
+
+        pq.push(max_work - 1);
+        n--;
+    }
+
+    long long answer = 0;
+
+    // add overtime index
+    while(!pq.empty()){
+        long long val = pq.top();
+        answer += val * val;
+        pq.pop();
+    }
+
+    return answer;
+}
+
+int main(){
+    auto result = solution(1, vector<int>{2, 1, 2});
+    cout << result << endl; 
+    return 0;
+}
